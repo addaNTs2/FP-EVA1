c27951ae8ec2c886df9d45f682ef83f3f77f8362
addaNTs2 <L24550283@chihuahua2.tecnm.mx>
2024-08-26 17:51:26 -0600

    Add files via upload

---
+++ EVA1_3_VARIABLES2/src/eva1_3_variables2/EVA1_3_VARIABLES2.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva1_3_variables2;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA1_3_VARIABLES2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //VEHICULOS
+        String marca;
+        String modelo;
+        int year; //La ñ en otros lenguajes no es aceptada
+        double precio;
+        
+        //ASIGNACION
+        marca = "Nissan";
+        modelo = "350z";
+        year = 2008;
+        precio = 80000;
+        
+        System.out.println(marca);
+        System.out.println(modelo);
+        System.out.println(year);
+        System.out.println(precio);
+        
+        
+        
+    }
+    
+}
